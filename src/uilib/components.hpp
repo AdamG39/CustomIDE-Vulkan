0ce73bdeb01ee61d90406746287037a1b256f264
AdamG39 <awgraham39@gmail.com>
2025-10-01 01:27:42 +0100

    Added button component [!UNTESTED]
    Copied most of old button logic to new component still need to hook it up with old event system to fix all the features that relied on that

--- src/uilib/components.hpp
+++ src/uilib/components.hpp
@@ -1,6 +1,8 @@
 #ifndef CUSTOM_COMPONENTS_H
 #define CUSTOM_COMPONENTS_H
 
+#include <functional>
+#include <cassert>
 #include "../renderer/shapes.hpp"
 #include "ui.hpp"
 
@@ -167,11 +169,48 @@ public:
   }
 };
 
-/*
 class Button : public IComponent {
+private:
+  std::function<void()> m_onPress;
+  std::function<void()> m_onRelease;
+  std::function<void()> m_onHover;
+
 public:
-  int GetType() override { return TypeButton; }
-};*/
+  static int TypeValue() { return TypeButton; }
+  int GetType() override { return TypeValue(); }
+
+  Button() = default;
+
+  template <typename Func, typename... Args>
+  void SetOnPress(Func&& Function, Args&&... Arguments) {
+    m_onPress = std::bind(std::forward(Function), std::decay<Args>(Arguments)...);
+  }
+
+  template <typename Func, typename... Args>
+  void SetOnRelease(Func&& Function, Args&&... Arguments) {
+    m_onRelease = std::bind(std::forward(Function), std::decay<Args>(Arguments)...);
+  }
+
+  template <typename Func, typename... Args>
+  void SetOnHover(Func&& Function, Args&&... Arguments) {
+    m_onHover = std::bind(std::forward(Function), std::decay<Args>(Arguments)...);
+  }
+
+  void OnPress() {
+    assert(m_onPress);
+    m_onPress();
+  }
+
+  void OnRelease() {
+    assert(m_onRelease);
+    m_onRelease();
+  }
+
+  void OnHover() {
+    assert(m_onHover);
+    m_onHover();
+  }
+};
 
 class Text : public IComponent {
 public:
