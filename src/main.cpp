bec66d99994fab214f69a6271b9d5807b8277afa
AdamG39 <awgraham39@gmail.com>
2026-07-21 00:35:46 +0100

    Changed CMakeLists to link as a standalone application when compiled in release mode

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,18 @@
 
 CustomIDE::Application* CustomIDE::Application::s_instance = nullptr;
 
-int main() {
+int main(int argc, char* argv[]) {
   CustomIDE::Application* app = CustomIDE::Application::GetInstance();
   app->InitApplication();
   app->RunApplication();
   app->EndApplication();
 }
+
+#ifdef _WIN32
+  #include <windows.h>
+
+  int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
+    return main(__argc, __argv);
+  }
+#endif
+
